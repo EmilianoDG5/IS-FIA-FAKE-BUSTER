7d829b7e75a2271445650807044a480efa89585a
bruv89 <b.santo@studenti.unisa.it>
2025-12-29 11:43:19 +0100

    modificato appello e aggiunta segnalazione

--- Code/app/models/appello.py
+++ Code/app/models/appello.py
@@ -1,8 +1,23 @@
 from app import db
+from datetime import datetime
 
 class Appello(db.Model):
-    __tablename__ = "appelli"
+    tablename = "appelli"
 
     id = db.Column(db.Integer, primary_key=True)
+
+    data_invio = db.Column(db.DateTime, default=datetime.utcnow)
+    stato = db.Column(db.String(20), nullable=False)   # aperto | chiuso
+    esito = db.Column(db.String(20))                   # accettato | respinto
     motivazione = db.Column(db.Text, nullable=False)
-    post_id = db.Column(db.Integer, db.ForeignKey("posts.id"), nullable=False)
+
+    post_id = db.Column(
+        db.Integer,
+        db.ForeignKey("posts.id"),
+        nullable=False
+    )
+
+    fact_checker_id = db.Column(
+        db.Integer,
+        db.ForeignKey("accounts.id")
+    )
---
+++ Code/app/models/segnalazione.py
@@ -0,0 +1,30 @@
+from app import db
+from datetime import datetime
+
+
+class Segnalazione(db.Model):
+    tablename = "segnalazioni"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    data_invio = db.Column(db.DateTime, default=datetime.utcnow)
+    stato = db.Column(db.String(20), nullable=False)   # aperta | chiusa
+    esito = db.Column(db.String(20))                   # valida | respinta
+    motivo = db.Column(db.String(255), nullable=False)
+
+    post_id = db.Column(
+        db.Integer,
+        db.ForeignKey("posts.id"),
+        nullable=False
+    )
+
+    fact_checker_id = db.Column(
+        db.Integer,
+        db.ForeignKey("accounts.id")
+    )
+
+    segnalatore_id = db.Column(
+        db.Integer,
+        db.ForeignKey("accounts.id"),
+        nullable=False
+    )
